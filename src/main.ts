42f3a9256960e49bddcd47235cdb6af35115d37c
teaarte <teaarte@gmail.com>
2024-04-14 20:01:58 +0200

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('Wish list API')
